727a3ebfea1ba7874df3019ae8b12197f7273d80
Tom Bugeja <46567954+TomBuge@users.noreply.github.com.>
2025-11-21 08:47:54 +0000

    Create getComputerChoice function
    function generates random number and returns
    either rock, paper, scissors to
    var computerChoice

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log("Hello World");
+
+/* 
+Create function getComputerChoice that returns rock if math.random equals <= 1/3, returns paper if 
+math.random > 1/3 and <= 2/3, else returns scissors. */ 
+
+let computerChoice = Math.floor(Math.random() * 3);
+
+console.log(computerChoice);
+
+function getComputerChoice() { 
+    if(computerChoice === 1) {
+        computerChoice = "rock";
+    } else if(computerChoice === 2 ) {
+        computerChoice = "paper";
+    } else{
+        computerChoice = "scissors";
+    }
+}
+
+getComputerChoice();
+console.log(computerChoice);
