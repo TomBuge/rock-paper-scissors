c45ca3398f866a3eb695dc28c1345b339273849a
Tom Bugeja <46567954+TomBuge@users.noreply.github.com.>
2025-11-28 12:53:14 +0000

    Add ui for game results
    add keepScore() and modify playRound() so that
    results and scores updated on webpage. add reset()
    to clear text once game finishes

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+console.log("test")
+let userScore = 0;
+let computerScore = 0;
+
+
 function getComputerChoice() { 
     let computerChoice = Math.floor(Math.random() * 3);
     if(computerChoice === 1) {
@@ -11,61 +16,123 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-function getUserChoice() {
-   let userChoice = prompt("Rock, Paper, Scissors??")
-    console.log(`You: ${userChoice}`); 
-    return userChoice;
-}
+  
 
 function playRound(user, computer) {
     user = user.toLowerCase();
     computer = computer.toLowerCase();
+
+
     if(user === "rock" && computer === "scissors") {
-        alert("You win! Rock beats Scissors");
         console.log("You win! Rock beats Scissors");
-        return "win";
+        return "You win! Rock beats Scissors";
     } else if(user === "paper" && computer === "rock") {
-        alert("You win! Paper beats Rock");
         console.log("You win! Paper beats Rock");
-        return "win";
+        return "You win! Paper beats Rock";
     } else if(user === "scissors" && computer === "paper") {
-        alert("You win! Scissors beats Paper");
         console.log("You win! Scissors beats Paper");
-        return "win";
+        return "You win! Scissors beats Paper";
     } else if(user === "rock" && computer === "paper") {
-        alert("You lose! Paper beats Rock");
         console.log("You lose! Paper beats Rock");
-        return "lose";
+        return "You lose! Paper beats Rock";
     } else if(user === "paper" && computer === "scissors") {
-        alert("You lose! Scissors beats Paper");
         console.log("You lose! Scissors beats Paper");
-        return "lose";
+        return "You lose! Scissors beats Paper";
     } else if(user === "scissors" && computer === "rock") {
-        alert("You lose! Rock beats Scissors");
         console.log("You lose! Rock beats Scissors");
-        return "lose";
+        return "You lose! Rock beats Scissors";
     } else {
-        alert("Honourable Draw!");
         console.log("Honourable Draw!");
-        return "draw";
+        return "Honourable Draw!"; 
+    }
+}
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((btn) => {
+    btn.addEventListener("click", (event) => {
+    const className = event.target.className;
+    console.log(`player: ${className}`);
+    let userChoice;
+    switch (className) {
+        case "rock":
+            userChoice = "rock";
+            break;
+        case "paper":
+            userChoice = "paper";
+            break;
+        case "scissors":
+            userChoice = "scissors";    
+    }
+    const result = playRound(userChoice, getComputerChoice());
+    keepScore(result);
+    });
+});
+
+let score = document.querySelector(".score");
+ 
+  
+
+function keepScore(result) {
+
+
+    if (result.includes("win")) {
+        userScore++;    
+    }
+    if (result.includes("lose")) {
+        computerScore++;
+        
+    }
+    if (result.includes("Draw")) {
+        computerScore++;
+        userScore++;    
+    }
+
+    score.textContent = `Player: ${userScore}   Computer: ${computerScore}`;
+    score.style.border = "2px solid black";
+
+
+    let message = document.querySelector(".results");
+    const para = document.createElement("p");
+    message.appendChild(para);
+    message.style.border = "2px solid black"
+    para.textContent = `${result} - ${score.textContent}`;
+
+    if (computerScore === 5 && userScore === 5) {
+        score.textContent = `It's a draw! ${computerScore} - ${userScore}`
+        setTimeout(() => {
+            alert(score.textContent);
+            reset(message, score);
+        }, 0);  
+        
+    }
+    if (computerScore === 5) {
+        score.textContent = `You Lose! ${computerScore} - ${userScore}`;
+        setTimeout(() => {
+            alert(score.textContent);    
+            reset(message, score);
+        }, 0);
+        
+    }
+    if (userScore === 5) {
+        score.textContent = `You Win! ${userScore} - ${computerScore}`;
+            setTimeout(() => {
+                alert(score.textContent);
+                reset(message, score);
+        }, 0);
+        
     }
 }
 
-/*function playGame() {
-    let userScore = 0;
-    let computerScore = 0;
+function reset(message, score) {
+    userScore = 0;
+    computerScore = 0;
+    message.textContent = "";
+    score.textContent = "";
+    message.style.border = "";
+    score.style.border = "";
     
-    for (let i = 0; i < 5; i++) {
-        const result = playRound(getUserChoice(), getComputerChoice());
-        
-        if (result === "win") userScore++;
-        if(result === "lose") computerScore++;
-        
-
-        console.log(`Your score: ${userScore}`);
-        console.log(`Computer score: ${computerScore}`);
-    }
 }
+    
 
-playGame(); 
+    
 
